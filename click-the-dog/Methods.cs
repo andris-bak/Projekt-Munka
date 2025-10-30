5837d081661e7a8fa184b77dc00977a25f2addf2
andris-bak <bakandris@gmail.com>
2025-10-30 14:53:51 +0100

    Resistance és DamageType

--- click-the-dog/Methods.cs
+++ click-the-dog/Methods.cs
@@ -3,28 +3,5 @@ using System;
 
 public partial class Methods : Node
 {
-	public GameManager GM;
-	private Main _mainScene;
-	public void HealthRegenHandler(Main mainScene)
-	{
-		_mainScene = mainScene;
-		GD.Print("HealthRegenHandler sikeresen példányosítva.");
-	}
-
-	public void RegenerateHealth()
-	{
-		GameManager GM = _mainScene.GM;
-
-		if (GM.HP < GM.MaxHP)
-		{
-			GM.HP = GM.HP + GM.Tick;
-		   
-			if (GM.HP > GM.MaxHP)
-			{
-				GM.HP = GM.MaxHP;
-			}
-			
-			_mainScene.UpdateHP();
-		}
-	}
+	
 }
--- click-the-dog/scripts/Enemy.cs
+++ click-the-dog/scripts/Enemy.cs
@@ -7,12 +7,26 @@ public partial class Enemy : Node
 	public int Health {get; private set;}
 	Random rnd = new Random();
 	
+	public enum DefenseType
+	{
+		NONE,
+		FIRE,
+		WATER,
+		AIR,
+		EARTH
+	}
+	
+	public DefenseType EnemyResistance {get; set;}
+	
 	public Enemy()
 	{
 		Level = 1;
 		
 		Health = rnd.Next(10,21);
 		
+		EnemyResistance = DefenseType.NONE;
+		
 	}
+
 	
 }
